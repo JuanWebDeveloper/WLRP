430d9620cfd54f221f325caac71c2be902ea45a3
Juan Vargas <juandeveloper19@gmail.com>
2023-02-20 01:12:48 -0500

    [FEAT] Creation of the component for the about page

---
+++ pages/about.tsx
@@ -0,0 +1,40 @@
+const AboutUs = () => {
+  return (
+    <div className='about-us'>
+      <div className='content'>
+        <h2>About Us</h2>
+        <p>
+          We are a group of passionate cooks and food enthusiasts who believe that everyone should have access to
+          delicious, healthy, and easy-to-make recipes. Our goal is to inspire you to cook more often and experiment
+          with new flavors and ingredients.
+        </p>
+        <p>
+          Whether you´re a beginner cook or an experienced home chef, our website has something for everyone. We offer a
+          wide range of recipes that are easy to follow and don´t require any fancy equipment or special skills. From
+          quick weeknight dinners to decadent desserts, we´ve got you covered.
+        </p>
+        <p>
+          Our team of recipe developers carefully tests and refines each recipe to ensure that it is not only delicious
+          but also foolproof. We believe that cooking should be a fun and rewarding experience, and we want to make it
+          as easy as possible for you to achieve great results in the kitchen.
+        </p>
+        <p>
+          At our website, we also believe that food is more than just nourishment - it´s a way to connect with others
+          and to celebrate life´s special moments. That´s why we offer recipes for every occasion, whether you´re
+          hosting a dinner party, celebrating a birthday, or just enjoying a quiet night in.
+        </p>
+        <p>
+          We also believe in the importance of using fresh, wholesome ingredients whenever possible. That´s why we
+          prioritize using seasonal produce and whole foods in our recipes. We want to help you make healthy choices
+          that are good for both your body and the planet.
+        </p>
+        <p>
+          Thank you for choosing our website as your go-to source for recipes. We hope that you enjoy cooking and eating
+          our recipes as much as we do!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
