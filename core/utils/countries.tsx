b03a9c17c8ba31aaadc616a2f4428f122d49c9b9
Juan Vargas <juandeveloper19@gmail.com>
2023-02-26 07:01:44 -0500

    [FEAT] Update of the list of countries for filtering

--- core/utils/countries.tsx
+++ core/utils/countries.tsx
@@ -3,86 +3,110 @@ interface Country {
   image: string;
 }
 
-export const countries: Country[] = [
+export const countries = [
   {
-    name: 'Argentina',
-    image: 'https://cdn.countryflags.com/thumbs/argentina/flag-400.png',
-  },
-  {
-    name: 'Brazil',
-    image: 'https://cdn.countryflags.com/thumbs/brazil/flag-400.png',
-  },
-  {
-    name: 'Canada',
-    image: 'https://cdn.countryflags.com/thumbs/canada/flag-400.png',
-  },
-  {
-    name: 'China',
-    image: 'https://cdn.countryflags.com/thumbs/china/flag-400.png',
-  },
-  {
-    name: 'France',
-    image: 'https://cdn.countryflags.com/thumbs/france/flag-400.png',
-  },
-  {
-    name: 'Germany',
-    image: 'https://cdn.countryflags.com/thumbs/germany/flag-400.png',
-  },
-  {
-    name: 'Italy',
-    image: 'https://cdn.countryflags.com/thumbs/italy/flag-400.png',
-  },
-  {
-    name: 'Japan',
-    image: 'https://cdn.countryflags.com/thumbs/japan/flag-400.png',
-  },
-  {
-    name: 'Mexico',
-    image: 'https://cdn.countryflags.com/thumbs/mexico/flag-400.png',
-  },
-  {
-    name: 'Russia',
-    image: 'https://cdn.countryflags.com/thumbs/russia/flag-400.png',
-  },
-  {
-    name: 'Spain',
-    image: 'https://cdn.countryflags.com/thumbs/spain/flag-400.png',
-  },
-  {
-    name: 'United Kingdom',
-    image: 'https://cdn.countryflags.com/thumbs/united-kingdom/flag-400.png',
-  },
-  {
-    name: 'United States',
+    name: 'American',
     image: 'https://cdn.countryflags.com/thumbs/united-states-of-america/flag-400.png',
   },
   {
-    name: 'Colombia',
+    name: 'Argentinian',
+    image: 'https://cdn.countryflags.com/thumbs/argentina/flag-400.png',
+  },
+  {
+    name: 'Brazilian',
+    image: 'https://cdn.countryflags.com/thumbs/brazil/flag-400.png',
+  },
+  {
+    name: 'British',
+    image: 'https://cdn.countryflags.com/thumbs/united-kingdom/flag-400.png',
+  },
+  {
+    name: 'Canadian',
+    image: 'https://cdn.countryflags.com/thumbs/canada/flag-400.png',
+  },
+  {
+    name: 'Chinese',
+    image: 'https://cdn.countryflags.com/thumbs/china/flag-400.png',
+  },
+  {
+    name: 'Colombian',
     image: 'https://cdn.countryflags.com/thumbs/colombia/flag-400.png',
   },
   {
-    name: 'Australia',
-    image: 'https://cdn.countryflags.com/thumbs/australia/flag-400.png',
+    name: 'Dutch',
+    image: 'https://cdn.countryflags.com/thumbs/netherlands/flag-400.png',
   },
   {
-    name: 'India',
+    name: 'Egyptian',
+    image: 'https://cdn.countryflags.com/thumbs/egypt/flag-400.png',
+  },
+  {
+    name: 'French',
+    image: 'https://cdn.countryflags.com/thumbs/france/flag-400.png',
+  },
+  {
+    name: 'Greek',
+    image: 'https://cdn.countryflags.com/thumbs/greece/flag-400.png',
+  },
+  {
+    name: 'Indian',
     image: 'https://cdn.countryflags.com/thumbs/india/flag-400.png',
   },
   {
-    name: 'South Africa',
-    image: 'https://cdn.countryflags.com/thumbs/south-africa/flag-400.png',
+    name: 'Irish',
+    image: 'https://cdn.countryflags.com/thumbs/ireland/flag-400.png',
   },
   {
-    name: 'South Korea',
-    image: 'https://cdn.countryflags.com/thumbs/south-korea/flag-400.png',
+    name: 'Italian',
+    image: 'https://cdn.countryflags.com/thumbs/italy/flag-400.png',
   },
   {
-    name: 'Switzerland',
-    image: 'https://cdn.countryflags.com/thumbs/switzerland/flag-400.png',
+    name: 'Jamaican',
+    image: 'https://cdn.countryflags.com/thumbs/jamaica/flag-400.png',
   },
   {
-    name: 'Sweden',
-    image: 'https://cdn.countryflags.com/thumbs/sweden/flag-400.png',
+    name: 'Japanese',
+    image: 'https://cdn.countryflags.com/thumbs/japan/flag-400.png',
+  },
+  {
+    name: 'Kenyan',
+    image: 'https://cdn.countryflags.com/thumbs/kenya/flag-400.png',
+  },
+  {
+    name: 'Malaysian',
+    image: 'https://cdn.countryflags.com/thumbs/malaysia/flag-400.png',
+  },
+  {
+    name: 'Mexican',
+    image: 'https://cdn.countryflags.com/thumbs/mexico/flag-400.png',
+  },
+  {
+    name: 'Moroccan',
+    image: 'https://cdn.countryflags.com/thumbs/morocco/flag-400.png',
+  },
+  {
+    name: 'Russian',
+    image: 'https://cdn.countryflags.com/thumbs/russia/flag-400.png',
+  },
+  {
+    name: 'Spanish',
+    image: 'https://cdn.countryflags.com/thumbs/spain/flag-400.png',
+  },
+  {
+    name: 'Thai',
+    image: 'https://cdn.countryflags.com/thumbs/thailand/flag-400.png',
+  },
+  {
+    name: 'Tunisian',
+    image: 'https://cdn.countryflags.com/thumbs/tunisia/flag-400.png',
+  },
+  {
+    name: 'Turkish',
+    image: 'https://cdn.countryflags.com/thumbs/turkey/flag-400.png',
+  },
+  {
+    name: 'Vietnamese',
+    image: 'https://cdn.countryflags.com/thumbs/vietnam/flag-400.png',
   },
 ];
 
