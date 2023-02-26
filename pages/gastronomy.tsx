8730fb1aa342032fb60b1455d752544d4a87dedd
Juan Vargas <juandeveloper19@gmail.com>
2023-02-26 07:07:54 -0500

    [FEAT] Bringing the initial information in the section of gastronomy by country

--- pages/gastronomy.tsx
+++ pages/gastronomy.tsx
@@ -1,8 +1,17 @@
-import { countries } from '@/core/utils/countries';
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { countries } from '@/core/utils/countries';
+import { Recipes } from '@/core/models/Recipes';
+import { mapRecipeByCountry } from '@/core/mappers/mapperRecipesData';
 
-const GastronomyByCountry = () => {
+const GastronomyByCountry = (props: any) => {
+  const { country, recipes }: any = props;
+  const [grid, setGrid] = useState('fourParts');
+  const [cardsContent, setCardsContent] = useState(recipes);
+  const [noRecipe, setNoRecipe] = useState({ search: '', noRecipe: false });
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className='gastronomy-country'>
       <div className='content'>
@@ -25,7 +34,15 @@ export default GastronomyByCountry;
 
 export async function getServerSideProps(context: any) {
   const { country }: any = context.query;
+
+  const response: Response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`);
+
+  const data: any = await response
+    .json()
+    .then((response) => response.meals)
+    .catch((e) => console.log(e));
+  const recipes: Recipes[] = data?.map((recipe: Object) => mapRecipeByCountry(recipe));
   return {
-    props: { country },
+    props: { country, recipes },
   };
 }
