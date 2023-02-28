fc07b2b2f69780e93615ea8c58332d60a62b738f
Juan Vargas <juandeveloper19@gmail.com>
2023-02-28 04:01:28 -0500

    [FEAT] The most frequently asked questions and their respective answers were compiled into an array

---
+++ core/utils/frequentlyQuestions.ts
@@ -0,0 +1,60 @@
+interface FrequentlyQuestions {
+  question: string;
+  answer: string;
+}
+
+export const frequentlyQuestions: FrequentlyQuestions[] = [
+  {
+    question: 'How can I search for recipes on your website?',
+    answer: 'You can search for recipes using our search bar on the homepage or by exploring our recipe categories.',
+  },
+  {
+    question: 'How can I find out what ingredients I need for a specific recipe?',
+    answer: 'The required ingredients for each recipe are listed on the individual recipe page.',
+  },
+  {
+    question: 'Can I submit my own recipes to be shared on your website?',
+    answer:
+      "Yes, we would love to see your recipes! Please submit your recipes using our contact form, and we'll review them for inclusion on our website if they meet our quality guidelines.",
+  },
+  {
+    question: 'How can I get in touch with your team if I have a question or concern?',
+    answer:
+      "You can contact us through the 'Contact Us' page on our website or by emailing us at wlrp@example.com. We'll do our best to respond to your inquiry as soon as possible.",
+  },
+  {
+    question: 'How do you ensure the quality and accuracy of the recipes on your website?',
+    answer:
+      'Our team carefully selects and tests each recipe before publishing it on our website. We also rely on user feedback to continually improve and update our content.',
+  },
+  {
+    question: 'Can I use your recipes for commercial purposes?',
+    answer:
+      'No, our recipes are for personal use only and should not be used for commercial purposes without our written permission.',
+  },
+  {
+    question: 'Do you offer nutritional information for your recipes?',
+    answer:
+      "Yes, nutritional information is available for most of our recipes. Simply scroll down to the 'Nutrition Information' section on each recipe page.",
+  },
+  {
+    question: 'Are your recipes suitable for people with specific dietary needs?',
+    answer:
+      'We offer a range of recipes to suit different dietary needs, including vegan, vegetarian, gluten-free, and low-carb options. However, we recommend that you consult with a medical professional before making any significant changes to your diet.',
+  },
+  {
+    question: 'Can I modify your recipes to suit my tastes?',
+    answer:
+      'Of course! We encourage you to experiment with our recipes and make them your own. However, please note that we cannot guarantee the results if you make significant modifications to the original recipe.',
+  },
+  {
+    question: 'Can I save recipes to access offline?',
+    answer:
+      'Unfortunately, we do not currently offer an offline saving feature. However, you can bookmark our website on your device to access it easily in the future.',
+  },
+  {
+    question: 'Do you have a mobile app for your recipes?',
+    answer:
+      'We currently do not have a mobile app, but our website is optimized for mobile viewing. Simply visit our website on your mobile device to access our recipes on the go.',
+  },
+];
