90fea4a5863485576659019f0e819840348aad9a
Juan Vargas <juandeveloper19@gmail.com>
2023-02-28 05:38:02 -0500

    [FEAT] Functionality has been added to show and hide the answers to questions on the FAQs page

--- pages/faqs.tsx
+++ pages/faqs.tsx
@@ -1,6 +1,22 @@
 import { frequentlyQuestions } from '@/core/utils/frequentlyQuestions';
 
 const FAQS = () => {
+  const toggleAnswer = (event: any): void => {
+    const currentQuestionAnswer = event.target.parentNode.querySelector('.answer');
+    const answers = document.querySelectorAll('.answer');
+    const questions = document.querySelectorAll('.question');
+
+    answers.forEach((answer, i) => {
+      if (answer == currentQuestionAnswer) {
+        answer.classList.toggle('active');
+        questions[i].classList.toggle('active');
+      } else {
+        answer.classList.remove('active');
+        questions[i].classList.remove('active');
+      }
+    });
+  };
+
   return (
     <div className='faqs'>
       <h2>
@@ -9,7 +25,7 @@ const FAQS = () => {
       <div className='content'>
         {frequentlyQuestions.map((faq, i) => (
           <div key={i} className='faq'>
-            <div className='question'>
+            <div className='question' onClick={toggleAnswer}>
               {faq.question} <i className='fas fa-plus'></i>
             </div>
             <div className='answer'>{faq.answer}</div>
