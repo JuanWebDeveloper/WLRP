d5d33d650ad91878e609dd533f5af3c8d4106da6
Juan Vargas <juandeveloper19@gmail.com>
2023-03-03 02:01:32 -0500

    [FEAT] A click event has been added to the labels so that each time they are clicked, focus is added to their corresponding input

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,4 +1,19 @@
+import { MouseEvent } from 'react';
+
 const ContactUs = () => {
+  const labelClick = (e: MouseEvent<HTMLLabelElement>): void => {
+    const target: HTMLLabelElement = e.target as HTMLLabelElement;
+    const parent: HTMLDivElement = target.parentNode as HTMLDivElement;
+
+    if (parent.querySelector('textarea')) {
+      const boundTextare: HTMLTextAreaElement = parent.querySelector('textarea') as HTMLTextAreaElement;
+      boundTextare.focus();
+    } else {
+      const boundInput: HTMLInputElement = parent.querySelector('input') as HTMLInputElement;
+      boundInput.focus();
+    }
+  };
+
   return (
     <div className='contact-us'>
       <div className='content'>
@@ -10,32 +25,39 @@ const ContactUs = () => {
             Do you have any questions, comments, or suggestions you&apos;d like to share with us? We&apos;d love to hear from you
             and help in any way we can.
           </p>
-          <p>
-            Please use the following form to get in touch with us. You can also email us directly at our email address or give us
-            a call. We&apos;ll get back to you as soon as possible.
-          </p>
+          <p>Please use the following form to get in touch with us We&apos;ll get back to you as soon as possible.</p>
           <h3>Thank you for your interest in our company!</h3>
         </div>
         <form className='contact-form' autoComplete='off'>
           <div className='form-item_container'>
             <input type='text' name='fullName' placeholder=' ' />
-            <label htmlFor='fullName'>Full name</label>
+            <label htmlFor='fullName' onClick={labelClick}>
+              Full name
+            </label>
           </div>
           <div className='form-item_container'>
             <input type='text' name='lastName' placeholder=' ' />
-            <label htmlFor='lastName'>Last name</label>
+            <label htmlFor='lastName' onClick={labelClick}>
+              Last name
+            </label>
           </div>
           <div className='form-item_container'>
             <input type='email' name='email' placeholder=' ' />
-            <label htmlFor='email'>Email</label>
+            <label htmlFor='email' onClick={labelClick}>
+              Email
+            </label>
           </div>
           <div className='form-item_container'>
             <input type='text' name='phone' placeholder=' ' />
-            <label htmlFor='phone'>Phone</label>
+            <label htmlFor='phone' onClick={labelClick}>
+              Phone
+            </label>
           </div>
           <div className='form-item_container'>
             <textarea name='message' placeholder=' '></textarea>
-            <label htmlFor='message'>Message</label>
+            <label htmlFor='message' onClick={labelClick}>
+              Message
+            </label>
           </div>
           <button className='button-default'>Send</button>
         </form>
